712beb89ec467b2dc5b3d3e551ab48a50ccdabba
Rodrigo <rodrigo@hal>
2016-01-05 13:55:53 +0100

    Documentar algoritmo de Simon

--- python/qc.py
+++ python/qc.py
@@ -131,20 +131,30 @@ class QMeasure:
 		self.vn = np.arange(len(vp))[vp!=0]
 
 	def collapse(self):
-		return np.random.choice(self.vn, p=self.vp)
+		return int(np.random.choice(self.vn, p=self.vp))
 
 class QProfiler:
 	def __init__(self, steps=100, timeout=60*5):
 		self.timeout = timeout
 		self.steps = steps
 
-	def start(self, code, setup='', title=None):
+	def start(self, code, setup='', title=None, fresh=True):
 		timing = np.zeros(self.steps)
 		tic = time.clock()
-		for i in range(self.steps):
-			timing[i] = self.time_func(code, setup)
-			toc = time.clock()
-			if toc-tic > self.timeout: break
+		if fresh:
+			for i in range(self.steps):
+				timing[i] = self.time_func(code, setup)
+				toc = time.clock()
+				if toc-tic > self.timeout: break
+		else:
+			exec(setup)
+			for i in range(self.steps):
+				ftic = time.clock()
+				exec(code)
+				ftoc = time.clock()
+				timing[i] = ftoc-ftic
+				toc = time.clock()
+				if toc-tic > self.timeout: break
 
 		timing = timing[0:i+1]
 		mean = np.mean(timing)
@@ -162,6 +172,78 @@ class QProfiler:
 		toc = time.clock()
 		return toc-tic
 
+class CPostprocess():
+	def __init__(self, bits, qm):
+		self.bits = bits
+		self.qm = qm
+
+	def bitfield(self, a):
+		bits = self.bits
+		r = np.zeros(bits, dtype='int')
+		for i in range(bits):
+			if a & 1<<i:
+				r[bits-i-1] = 1
+
+		return r
+
+	def solve(self, s):
+		n = 2**self.bits
+		for i in range(0, n):
+			ok = True
+			for j in s:
+				x = i & j
+				xbin = self.bitfield(x)
+				if(list(xbin).count(1) % 2 == 1):
+					ok = False
+					break
+			if not ok: continue
+			#print('Solution {}'.format(i))
+
+	def run(self):
+		'Ejecuta la simulación de forma aleatoria'
+
+		lin_ind = set()
+		accum = set()
+		steps = 0
+		while True:
+			if len(lin_ind) >= self.bits - 1: break
+			y = self.qm.collapse()
+			steps += 1
+			if (y == 0) or (y in accum):
+				#print('Vector miss {}'.format(y))
+				continue
+			tmp = accum.copy()
+			for v in tmp:
+				accum.add(v^y)
+			accum.add(y)
+			lin_ind.add(y)
+			#print(lin_ind)
+
+		#print('{} steps'.format(steps))
+		#self.solve(lin_ind)
+		return steps
+
+	def profile(self, loops=100, timeout=60, title=None):
+		vstep = np.zeros(loops)
+		tic = time.clock()
+
+		for i in range(loops):
+			vstep[i] = self.run()
+			toc = time.clock()
+			if(toc - tic > timeout): break
+
+		vstep = vstep[0:i+1]
+		mean = np.mean(vstep)
+		var = np.var(vstep)
+		steps = len(vstep)
+
+		title_str = ''
+		if title: title_str = '%s: ' % title
+		print("{}Mean {:.3e}, var {:.3e}, steps {}"
+			.format(title_str, mean,var,steps))
+
+
+
 def profile():
 	Nmin = 2
 	Nmax = 9
@@ -169,24 +251,36 @@ def profile():
 
 	qp = QProfiler(timeout=Tmax)
 
-	print("Profile of QSimon.init()")
-	for i in range(Nmin, Nmax+1):
-		qp.start('QSimon(config)',
-			'config={"bits":%d}'%i,
-			title='N = %d'%i)
+#	print("Profile of QSimon.init()")
+#	for i in range(Nmin, Nmax+1):
+#		qp.start('QSimon(config)',
+#			'config={"bits":%d}'%i,
+#			title='N = %d'%i)
+#
+#	print("Profile of QSimon.run()")
+#	for i in range(Nmin, Nmax+1):
+#		setup = ('qs = QSimon({"bits":%d});'%i) + \
+#				'config = qs.random_config(%d)'%i
+#		qp.start('qs.run(config)', setup, title='N = %d'%i)
+#
+#	print("Profile of QMeasure()")
+#	for i in range(Nmin, Nmax+1):
+#		setup = ('qs = QSimon({"bits":%d});'%i) + \
+#				'config = qs.random_config(%d);'%i + \
+#				'st = qs.run(config)'
+#		qp.start('QMeasure(st, np.arange(%d, %d*2))'%(i,i), setup, title='N = %d'%i)
+#
+#	print("Profile of QMeasure.collapse()")
+#	for i in range(Nmin, Nmax+1):
+#		setup = ('qs = QSimon({"bits":%d});'%i) + \
+#				'config = qs.random_config(%d);'%i + \
+#				'st = qs.run(config);' + \
+#				'qm = QMeasure(st, np.arange(%d, %d*2))'%(i,i)
+#		qp.start('qm.collapse()', setup, title='N = %d'%i, fresh=False)
 
-	print("Profile of QSimon.run()")
-	for i in range(Nmin, Nmax+1):
-		setup = ('qs = QSimon({"bits":%d});'%i) + \
-				'config = qs.random_config(%d)'%i
-		qp.start('qs.run(config)', setup, title='N = %d'%i)
-
-	print("Profile of QMeasure()")
-	for i in range(Nmin, Nmax+1):
-		setup = ('qs = QSimon({"bits":%d});'%i) + \
-				'config = qs.random_config(%d);'%i + \
-				'st = qs.run(config)'
-		qp.start('QMeasure(st, np.arange(%d, %d*2))'%(i,i), setup, title='N = %d'%i)
+	print("Profile of np.random.choice()")
+	for i in range(Nmin, 60):
+		qp.start('np.random.choice(2**(%d-1))'%i, '', title='N = %d'%i)
 
 
 def main():
@@ -194,11 +288,22 @@ def main():
 	#if len(sys.argv) == 2: N = int(sys.argv[1])
 	#print("N = %d" % N)
 
-	N = 2
-	qs = QSimon({"bits":2})
-	st = qs.run({'f':np.array([0, 0, 1, 1])})
-	qm = QMeasure(st, np.arange(N, N*2))
+	#N = 2
+	#qs = QSimon({"bits":2})
+	#st = qs.run({'f':np.array([0, 0, 1, 1])})
+	#qm = QMeasure(st, np.arange(N, N*2))
+	#cp = CPostprocess(2, qm)
+	#cp.run()
 
+	#N = 9
+	for N in range(2, 11):
+		qs = QSimon({"bits":N})
+		config = qs.random_config(N)
+		#print(config)
+		st = qs.run(config)
+		qm = QMeasure(st, np.arange(N, N*2))
+		cp = CPostprocess(N, qm)
+		cp.profile(10000, title='N = %d'%N)
 
 	#st = qs.run({'f':np.array([1, 0, 1, 0])})
 	##config = qs.random_config(N)
@@ -209,5 +314,5 @@ def main():
 	#rnd = np.random.choice(qm.vn, p=qm.vp, size=100)
 	##print(qm.collapse())
 
-#main()
-profile()
+main()
+#profile()
