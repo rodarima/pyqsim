dd41ce41985970642a97e76873a348e84a28c48c
Rodrigo Arias <rodarima@gmail.com>
2016-01-26 23:05:47 +0100

    Monitorizar ficheros icluidos con \input{}

--- doc/latex.sh
+++ doc/latex.sh
@@ -4,18 +4,20 @@ FILE=$1
 TEX=${FILE}.tex
 PDF=${FILE}.pdf
 
-FILES=$(cat "$TEX" |\
-	grep -o '\\input{.*}' |\
-	sed -e 's/\\input{\(.*\)}/\1.tex/g' |\
-	paste -sd " ")
 
 while [ 1 ]; do
+	# Files to look can change
+	FILES=$(cat "$TEX" |\
+		grep -o '\\input{.*}' |\
+		sed -e 's/\\input{\(.*\)}/\1.tex/g' |\
+		paste -sd " ")
+
 	inotifywait -e modify $TEX $FILES
+
 	pdflatex -interaction=nonstopmode -halt-on-error $TEX
 	if [ "$?" != "0" ]; then
 		echo -e "\a"
 		continue
 	fi
-	kill -HUP $(pidof mupdf)
-	kill -HUP $(pidof mu)
+	kill -HUP $(pidof mupdf) $(pidof mu)
 done
